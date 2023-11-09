165922fe0e2ebb77c3b522afddb9da29fcdc7e6a
xshinhe <xshinhe@pku.edu.cn>
2023-11-09 21:55:19 +0800

    update

--- kernels/Kernel_Elec_SH.cpp
+++ kernels/Kernel_Elec_SH.cpp
@@ -35,7 +35,7 @@ int Kernel_Elec_SH::pop_choose(num_complex* rho) {
     num_real sum = 0.0f;
     Kernel_Random::rand_uniform(&rand_tmp);
     for (int i = 0, ii = 0; i < Dimension::F; ++i, ii += Dimension::Fadd1) {
-        sum += std::min({std::max({std::real(rho[ii]), 0}), 1});
+        sum += std::min({std::max({std::real(rho[ii]), 0.0e0}), 1.0e0});
         if (rand_tmp < sum) return i;
     }
     return 0;
