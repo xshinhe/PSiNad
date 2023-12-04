067090f57687349509a5ab8af24dd360b5a5e392
xshinhe <xshinhe@pku.edu.cn>
2023-12-04 10:16:04 +0800

    update ns

--- core/ns.cpp
+++ core/ns.cpp
@@ -2,32 +2,63 @@
 
 #include <iostream>
 
+#include "DataSet_test.h"
+
 // #include "types.h"
 
-#define DEF_VARIABLE(TYPE, name, doc) VARIABLE<TYPE> name = VARIABLE<TYPE>(#name, doc);
+#define DEF_VARIABLE(TYPE, name, shape, doc) VARIABLE<TYPE> name = VARIABLE<TYPE>(#name, shape, doc);
+#define OPENDF_DECLARE_VARIABLE(type, name) \
+    namespace name {                        \
+    VARIABLE<type> var;                     \
+    };
+
+#define OPENDF_DEFINE_VARIABLE(type, name, shape, doc) \
+    namespace name {                                   \
+    VARIABLE<type> var(#name, shape, doc);             \
+    };
 
 namespace PROJECT_NS {
 
+namespace apple::banana {
+int c = 0;
+};  // namespace apple::banana
 
 namespace DATASET {
+std::vector<void*> _VAR_LIST;
 
-DEF_VARIABLE(int, dimension::M, "Dimension of Monte Carlo Size");
-DEF_VARIABLE(int, dimension::P, "Parallel");
-DEF_VARIABLE(double, dimension::x, "coordinate");
-DEF_VARIABLE(double, integrator::baoab::x, "coordinate 2");
+namespace DIM {
+DSize P;
+DSize N;
+DSize F;
+DSize M;
+Shape PN({&P, &N});
+};  // namespace DIM
+
+DEF_VARIABLE(double, dimension::x, &DIM::PN, "coordinate");
+DEF_VARIABLE(double, integrator::baoab::x, &DIM::PN, "coordinate 2");
+
+
+OPENDF_DEFINE_VARIABLE(double, integrator::x, &DIM::PN, "coordinates of integrator");
+OPENDF_DEFINE_VARIABLE(double, integrator::p, &DIM::PN, "coordinates of integrator");
+
 
 };  // namespace DATASET
 
 };  // namespace PROJECT_NS
 
-int main() {
-    std::cout << PROJECT_NS::DATASET::dimension::M.help() << "\n";
-    std::cout << PROJECT_NS::DATASET::dimension::M.name() << "\n";
+using namespace PROJECT_NS;
 
+int main() {
+    DataSet DS;
     for (auto& i : PROJECT_NS::DATASET::_VAR_LIST) {
         std::cout << ((PROJECT_NS::DATASET::VARIABLE<int>*) i)->name() << " : "
                   << ((PROJECT_NS::DATASET::VARIABLE<int>*) i)->help() << "\n";
     }
+    std::cout << DATASET::integrator::x::var.name() << "\n";
+
+    double* x = DS.reg(DATASET::integrator::x::var);
+
+
 
     return 0;
 }
--- core/ns.h
+++ core/ns.h
@@ -1,44 +1,42 @@
+#ifndef NS_H
+#define NS_H
+
 #include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
 
-#include "DataSet.h"
+#include "Shape.h"
 
 namespace PROJECT_NS {
 
 namespace DATASET {
 
-static std::vector<void*> _VAR_LIST;
+extern std::vector<void*> _VAR_LIST;
 
 template <class T>
 class VARIABLE {
    public:
-    VARIABLE(const std::string& name, const std::string& doc) : _name{name}, _doc{doc} {
+    VARIABLE(const std::string& name, Shape* shape, const std::string& doc) : _name{name}, _shape{shape}, _doc{doc} {
         std::string::size_type pos = 0;
         while ((pos = _name.find("::")) != std::string::npos) _name.replace(pos, 2, ".");
+        // _VARS.push_back(this);
         _VAR_LIST.push_back((void*) this);
-    }
-
-    T* init_data(DataSet* DS = nullptr, int size = 1) {
-        if (!allocated && DS != nullptr) _data = DS->reg<T>(_name, size);
-        allocated = true;
-        return _data;
     }
 
     std::string help() { return _doc; }
     std::string name() { return _name; }
+    std::size_t size() { return (*_shape)(); }
 
    private:
     std::string _name;
     T* _data;
+    Shape* _shape;
     std::string _doc;
     bool allocated = false;
 };
 
 namespace dimension {
-extern VARIABLE<int> M;     ///< No. of MonteCarlo
-extern VARIABLE<int> P;     ///< No. of MonteCarlo
 extern VARIABLE<double> x;  ///< No. of MonteCarlo
 };                          // namespace dimension
 
@@ -54,3 +52,5 @@ extern VARIABLE<double> f;
 };  // namespace DATASET
 
 };  // namespace PROJECT_NS
+
+#endif  // NS_H
