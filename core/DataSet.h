bb793280cb28639692eece64b0942b9516f5d490
xshinhe <xshinhe@pku.edu.cn>
2024-04-19 00:02:32 +0800

    update DataSet with smart pointers

--- core/DataSet.h
+++ core/DataSet.h
@@ -20,16 +20,6 @@
  *  You should have received a copy of the GNU Lesser General Public License along
  *  with this software. If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html>
  **********************************************************************************
- * @todo
- *  Implement a smart pointer version for the DataSet class. One possible approach
- *  is to use `std::unique_ptr<T[]>`, as demonstrated in the file:
- *      `DataSet.cppversion3_smartlymanage`.
- *  However, it should be noted that accessing data is not currently handled
- *  correctly with this implementation. On the other hand, `std::unique_ptr` is
- *  unsuitable for scenarios requiring shared access to the storage of the DataSet
- *  object. While `std::shared_ptr<T[]>` could potentially address this issue, it
- *  is not as well-supported in C++. Consequently, designing the DataSet class with
- *  `std::shared_ptr` is necessary.
  *
  *  Due to this limitation, storage is managed manually in current version. Copying
  *  a DataSet to another instance is prohibited, and reassigning a DataSet is also
@@ -42,6 +32,13 @@
  * <tr><td> 2024-03-29  <td> initial version. Seperate Shape class to another file.
  *                          Add help() function. Review more over smart pointers.
  *                          Improve print format.
+ * <tr><td> 2024-04-18  <td> move to smart pointers. Instead of shared_ptr<T[]>, we
+ *                          use shared_ptr<vector<T>> to manage memory.
+ *                          However, there are limits:
+ *                          1) vector<bool> is not STL, so it's not supported as a
+ *                              part of Tensor<T>.
+ *                          2)
+ *
  * </table>
  *
  **********************************************************************************
@@ -56,6 +53,7 @@
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <tuple>
 #include <type_traits>
 #include <vector>
@@ -91,31 +89,15 @@ constexpr inline int FMT_WIDTH(int X) { return X + 7; }
  */
 class Node {
    public:
-    using SizeType = std::size_t;
-    using DataType = void;
-
-    virtual ~Node() { _delete(); }  //< deconstructor
-
     virtual std::string repr() = 0;
 
     virtual std::string help(const std::string& name) = 0;
 
     inline kids_dtype type() { return _type; }
 
-    inline SizeType size() { return _size; }
-
-    inline DataType* data() { return _data; }
-
    protected:
     friend class DataSet;
-
-    kids_dtype _type      = kids_void_type;
-    SizeType   _size      = 0;
-    DataType*  _data      = nullptr;
-    bool       _ownership = false;
-
-   private:
-    virtual void _delete(){};  // virtual function called by deconstructor
+    kids_dtype _type = kids_void_type;
 };
 
 /**
@@ -130,15 +112,13 @@ class Tensor final : public Node {
     Tensor(Shape S, const std::string& info = "") : _shape{S}, _doc_info{info} {
         _type = as_enum<T>();
         _size = _shape.size();
-        _data = (void*) new T[_size];
-        memset(_data, 0, _size * sizeof(T));
-        _ownership = true;
+        _data = std::shared_ptr<std::vector<T>>(new std::vector<T>(_size, 0));
     }
-    virtual ~Tensor() { _delete(); }
 
     virtual std::string repr() {
         std::ostringstream os;
-        T*                 ptr = (T*) data();
+        // T*                 ptr = _data->data();
+        T* ptr = reinterpret_cast<T*>(_data->data());
         os << as_str<T>();
         os << FMT(0) << _size;
         os << "\n";
@@ -148,16 +128,17 @@ class Tensor final : public Node {
 
     virtual std::string help(const std::string& name) { return _doc_info; }
 
+    inline T* data() { return reinterpret_cast<T*>(_data->data()); }
+
+    inline std::size_t size() { return _size; }
+
     inline Shape& shape() { return _shape; }
 
    private:
-    Shape       _shape;
-    std::string _doc_info;
-
-    virtual void _delete() {
-        if (_ownership) delete[] static_cast<T*>(_data);
-        _ownership = false;
-    }
+    std::size_t                     _size;
+    Shape                           _shape;
+    std::string                     _doc_info;
+    std::shared_ptr<std::vector<T>> _data;
 };
 
 /**
@@ -169,40 +150,39 @@ class DataSet final : public Node {
     DataSet& operator=(const DataSet&) = delete;
 
    public:
-    using DataType = std::map<std::string, Node*>;
+    using DataType = std::map<std::string, std::shared_ptr<Node>>;
+    std::shared_ptr<DataType> _data;
 
     DataSet() {
-        _size      = 1;  // only one TensorBuffer will be newed
-        _type      = kids_dataset_type;
-        _data      = (void*) new DataType();
-        _ownership = true;
-    };
-
-    virtual ~DataSet() {
-        if (_ownership) _delete();
-        _ownership = false;
+        _type = kids_dataset_type;
+        _data = std::shared_ptr<DataType>(new DataType());
     }
 
     template <typename T>
     T* def(const std::string& key, Shape S = 1, const std::string& info = "") {
-        DataSetKeyParser kh    = DataSetKeyParser(key);
-        DataType*        d_ptr = static_cast<DataType*>(_data);
+        DataSetKeyParser          kh    = DataSetKeyParser(key);
+        std::shared_ptr<DataType> d_ptr = _data;
 
         DataSet* currentNode = this;
         for (size_t i = 0; i < kh.terms.size() - 1; ++i) {
             auto& node = (*d_ptr)[kh.terms[i]];
-            if (!node) node = new DataSet;
+            if (!node) node = std::shared_ptr<DataSet>(new DataSet());
 
-            currentNode = static_cast<DataSet*>(node);
-            d_ptr       = static_cast<DataType*>(currentNode->_data);
+            currentNode = static_cast<DataSet*>(node.get());
+            d_ptr       = currentNode->_data;
         }
 
-        auto& leaf_node = (*d_ptr)[kh.terms.back()];
-        if (!leaf_node) leaf_node = new Tensor<T>(S, info);
+        std::shared_ptr<Node>& leaf_node = (*d_ptr)[kh.terms.back()];
+        if (!leaf_node) leaf_node = std::shared_ptr<Tensor<T>>(new Tensor<T>(S, info));
 
-        if (leaf_node->type() != as_enum<T>() || S.size() != leaf_node->size())
+        if (leaf_node->type() != as_enum<T>()) {  //
             throw std::runtime_error("doubly conflicted definition!");
-        return static_cast<T*>(leaf_node->data());
+        }
+        auto leaf_node_ts = static_cast<Tensor<T>*>(leaf_node.get());
+        if (S.size() != leaf_node_ts->size()) {  //
+            throw std::runtime_error("doubly conflicted definition!");
+        }
+        return leaf_node_ts->data();
     }
 
     template <typename T>
@@ -212,12 +192,14 @@ class DataSet final : public Node {
         return arr;
     }
 
+    // bug here!!! when {...,...} it will pass into this function not the shape function
     template <typename T>
     T* def(const std::string& key, const std::string& key_in, const std::string& info = "") {
         auto inode = node(key_in);
         if (inode->type() == kids_dataset_type)
             throw std::runtime_error(std::string{key_in} + " : failed copying dataset");
-        return def<T>(key, (T*) inode->data(), inode->size(), info);
+        auto inode_ts = static_cast<Tensor<T>*>(inode);
+        return def<T>(key, inode_ts->data(), inode_ts->size(), info);
     }
 
     template <typename T>
@@ -234,9 +216,6 @@ class DataSet final : public Node {
     DataSet& _def(const std::string& key, const std::string& key_in, const std::string& info = "") {
         auto leaf_node = node(key_in);
         switch (leaf_node->type()) {
-            case kids_bool_type:
-                def<kids_bool>(key, key_in, info);
-                break;
             case kids_int_type:
                 def<kids_int>(key, key_in, info);
                 break;
@@ -253,22 +232,21 @@ class DataSet final : public Node {
     }
 
     DataSet& _undef(const std::string& key) {
-        DataSetKeyParser kh    = DataSetKeyParser(key);
-        DataType*        d_ptr = static_cast<DataType*>(_data);
+        DataSetKeyParser          kh    = DataSetKeyParser(key);
+        std::shared_ptr<DataType> d_ptr = _data;
 
         DataSet* currentNode = this;
         for (size_t i = 0; i < kh.terms.size() - 1; ++i) {
             auto& node = (*d_ptr)[kh.terms[i]];
             if (!node) return *this;
 
-            currentNode = static_cast<DataSet*>(node);
-            d_ptr       = static_cast<DataType*>(currentNode->_data);
+            currentNode = static_cast<DataSet*>(node.get());
+            d_ptr       = currentNode->_data;
         }
-
         auto it = d_ptr->find(kh.terms.back());
         if (it != d_ptr->end()) {
-            delete it->second;
-            d_ptr->erase(it);
+            std::cout << "\n";
+            it->second.reset();
         }
         return *this;
     }
@@ -279,11 +257,6 @@ class DataSet final : public Node {
             case kids_int_type: {
                 auto&& conv_node = static_cast<Tensor<kids_int>*>(leaf_node);
                 return std::make_tuple(kids_int_type, conv_node->data(), &(conv_node->shape()));
-                break;
-            }
-            case kids_bool_type: {
-                auto&& conv_node = static_cast<Tensor<kids_bool>*>(leaf_node);
-                return std::make_tuple(kids_bool_type, conv_node->data(), &(conv_node->shape()));
                 break;
             }
             case kids_real_type: {
@@ -304,21 +277,21 @@ class DataSet final : public Node {
     }
 
     Node* node(const std::string& key) {
-        DataSetKeyParser kh    = DataSetKeyParser(key);
-        DataType*        d_ptr = static_cast<DataType*>(_data);
+        DataSetKeyParser          kh    = DataSetKeyParser(key);
+        std::shared_ptr<DataType> d_ptr = _data;
 
         DataSet* currentNode = this;
         for (size_t i = 0; i < kh.terms.size() - 1; ++i) {
             auto& node = (*d_ptr)[kh.terms[i]];
             if (!node) throw std::runtime_error(std::string{key} + " : access undefined key!");
 
-            currentNode = static_cast<DataSet*>(node);
-            d_ptr       = static_cast<DataType*>(currentNode->_data);
+            currentNode = static_cast<DataSet*>(node.get());
+            d_ptr       = currentNode->_data;
         }
 
         auto& leaf_node = (*d_ptr)[kh.terms.back()];
         if (!leaf_node) throw std::runtime_error(std::string{key} + " : access undefined key!");
-        return leaf_node;
+        return leaf_node.get();
     }
 
     template <typename T = DataSet>
@@ -326,7 +299,7 @@ class DataSet final : public Node {
         auto leaf_node = node(key);
         if (leaf_node->type() != as_enum<T>()) throw std::runtime_error("bad conversion!");
         if (leaf_node->type() == kids_dataset_type) return (T*) (leaf_node);
-        return static_cast<T*>(leaf_node->data());
+        return static_cast<Tensor<T>*>(leaf_node)->data();
     }
 
     virtual std::string help(const std::string& name) {
@@ -341,11 +314,11 @@ class DataSet final : public Node {
             auto [parent, currentNode] = stack.back();
             stack.pop_back();
 
-            DataType* d_ptr = static_cast<DataType*>(currentNode->data());
+            std::shared_ptr<DataType> d_ptr = static_cast<DataSet*>(currentNode)->_data;
 
             for (auto& i : (*d_ptr)) {
                 std::string key   = (parent == "") ? i.first : parent + "." + i.first;
-                Node*       inode = i.second;
+                Node*       inode = i.second.get();
                 if (inode->type() == kids_dataset_type) {
                     stack.push_back(std::make_tuple(key, inode));
                 } else {
@@ -365,12 +338,12 @@ class DataSet final : public Node {
         while (!stack.empty()) {
             auto [parent, currentNode] = stack.back();
             stack.pop_back();
-
-            DataType* d_ptr = static_cast<DataType*>(currentNode->data());
-
+            std::shared_ptr<DataType> d_ptr = static_cast<DataSet*>(currentNode)->_data;
             for (auto& i : (*d_ptr)) {
-                std::string key   = (parent == "") ? i.first : parent + "." + i.first;
-                Node*       inode = i.second;
+                std::string key = (parent == "") ? i.first : parent + "." + i.first;
+                if (!i.second) continue;
+                Node* inode = i.second.get();
+
                 if (inode->type() == kids_dataset_type) {
                     stack.push_back(std::make_tuple(key, inode));
                 } else {
@@ -413,12 +386,6 @@ class DataSet final : public Node {
             terms.emplace_back(key, start);
         }
     };
-
-    virtual void _delete() {
-        DataType* d_ptr = static_cast<DataType*>(_data);
-        for (auto& i : (*d_ptr)) delete i.second;
-        delete d_ptr;  // essential!
-    }
 };
 
 };  // namespace PROJECT_NS
