7aa74d7122709b31abb739d8ab9a7bc4b5b28955
xshinhe <xshinhe@pku.edu.cn>
2024-12-30 22:07:22 +0800

    1230

--- src/core/include/kids/version.h
+++ src/core/include/kids/version.h
@@ -5,4 +5,4 @@
 #define kids kids
 
 constexpr char repo_name[] = "kids";
-constexpr char repo_version[] = "main-(2024-09-21)-2712d38e98fb61a90657fb42ae815838509135c2";
+constexpr char repo_version[] = "main-(2024-12-06)-8116ab5cd7b3403dca96ee0944a8a174017a3fe3";
